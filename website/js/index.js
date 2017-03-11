75af5c67d7d74870d10d003fe9e96d99497274c4
floatfeather <lilianwangran@163.com>
2017-03-11 11:10:25 +0800

    add news

--- website/js/index.js
+++ website/js/index.js
@@ -1,3 +1,55 @@
-$(document).ready(function($) {
-	
+$(function() {
+	function inflateNews(data) {
+		var $newsDiv = $('#news')
+		var newsTpl = Handlebars.compile($('#news-icon-template').html())
+
+		$.each(data.news, function(i, item){
+			if (i % 3 == 0) {
+				item['position'] = 'first'
+			} else if (i % 3 == 1) {
+				item['position'] = 'second'
+			} else {
+				item['position'] = 'third'
+			}
+		})
+
+		console.log(data)
+
+		$newsDiv.html(
+			newsTpl(
+				data
+			)
+		)
+	}
+
+	var data = {
+		"news": [
+			{
+				"imageURL": "http://images1.ocweekly.com/imager/u/745xauto/7950031/news.jpg",
+				"title": "FBI Used Best Buy's Geek Squad To Increase Secret Public Surveillance"
+			},
+			{
+				"imageURL": "http://images1.ocweekly.com/imager/u/745xauto/7950031/news.jpg",
+				"title": "FBI Used Best Buy's Geek Squad To Increase Secret Public Surveillance"
+			},
+			{
+				"imageURL": "http://images1.ocweekly.com/imager/u/745xauto/7950031/news.jpg",
+				"title": "FBI Used Best Buy's Geek Squad To Increase Secret Public Surveillance"
+			},
+			{
+				"imageURL": "http://images1.ocweekly.com/imager/u/745xauto/7950031/news.jpg",
+				"title": "FBI Used Best Buy's Geek Squad To Increase Secret Public Surveillance"
+			},
+			{
+				"imageURL": "http://images1.ocweekly.com/imager/u/745xauto/7950031/news.jpg",
+				"title": "FBI Used Best Buy's Geek Squad To Increase Secret Public Surveillance"
+			},
+			{
+				"imageURL": "http://images1.ocweekly.com/imager/u/745xauto/7950031/news.jpg",
+				"title": "FBI Used Best Buy's Geek Squad To Increase Secret Public Surveillance"
+			}
+		]
+	}
+
+	inflateNews(data)
 });
